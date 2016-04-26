26ae0f075cc378d9cef672965c50ba8c6567eea0
AjiMIde <adele513900383@gmail.com>
2016-04-26 19:23:02 +0800

    1，增加一个style.php样式表。2，增加一个随机函数说明

--- Learn/Util_funs.php
+++ Learn/Util_funs.php
@@ -11,6 +11,8 @@
  * # 传入参数的值（数组）
  * # 输入指定位置的参数
  */
+include_once "../keyCommon/style.php";
+
 function get() {
     print_r("<h4>Get the sum of params you pass with 'func_num_args': </h4>");
     print_r(func_num_args());
@@ -47,4 +49,21 @@ function parse_ini() {
 parse_ini();
 
 
+print_r("<h3>随机函数的使用：</h3>");
+function getRand() {
+    for ($i = 0; $i < 5; $i++) {
+        print_r("<p>" . rand(10, 30) . "</p>");
+    }
 
+    for ($i = 0; $i < 5; $i++) {
+        print_r("<p>" . mt_rand(1, 5) . "</p>");
+    }
+
+    $arr = array("wx", "sms", "rw");
+    for ($i = 0; $i < 5; $i++) {
+        $j = mt_rand(0, 2);
+        print_r("<p>" . $arr[$j] . "</p>");
+    }
+}
+
+getRand();
---
+++ keyCommon/style.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User        : Aij
+ * DateTime    : 2016/4/26-19:13
+ * Modified    : 2016/4/26-19:13
+ * Description :
+ */
+?>
+<html>
+    <title>Array Instance</title>
+    <head>
+        <style>
+body{font-family: "Meiryo UI","Helvetica Neue",Helvetica,Arial, "Hiragino Sans GB","Hiragino Sans GB W3", "Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei", sans-serif, \5FAE\8F6F\96C5\9ED1,\9ED1\4F53,\5b8b\4f53 !important}
+            h3{30px 0 1px 0}
+
+        </style>
+    </head>
+</html>
